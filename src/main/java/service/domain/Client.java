8f0a6165caefd8274ff3015d38fb6df53f1ec301
Wei Lun <weilun_95@hotmail.com>
2018-03-20 01:34:21 +0800

    added client class

--- src/main/java/service/domain/Client.java
+++ src/main/java/service/domain/Client.java
@@ -1,4 +1,34 @@
 package java.service.domain;
 
+import java.util.UUID;
+
 public class Client {
+
+    private String id;
+    private String name;
+    private String address;
+    private String contactNo;
+
+    public Client(String name, String address, String contactNo){
+        id = UUID.fromString(name).toString();
+        this.name = name;
+        this.address = address;
+        this.contactNo = contactNo;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getContactNo() {
+        return contactNo;
+    }
 }
