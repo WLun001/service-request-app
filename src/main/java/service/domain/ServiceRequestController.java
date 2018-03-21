249dc19a296d401db88abca01b5ea8ca87465303
Wei Lun <weilun_95@hotmail.com>
2018-03-21 09:23:09 +0800

    removed ignore file

---
+++ src/main/java/service/domain/ServiceRequestController.java
@@ -0,0 +1,39 @@
+package java.service.domain;
+
+import java.util.List;
+
+public class ServiceRequestController {
+    private IServiceRequestList requestList;
+    private ServiceRequest serviceRequest;
+
+    public ServiceRequestController(){
+        requestList = new ServiceRequestList();
+    }
+
+    public ServiceRequest getSelectedServiceRequest() {
+        return serviceRequest;
+    }
+
+    public void serviceRequest(String clientName, String clientAdd, String clientNo, String date){
+        requestList.addServiceRequest(new Client(clientName, clientAdd, clientNo), date);
+    }
+
+    public ServiceRequest searchServiceRequest(String requestId){
+        serviceRequest = requestList.searchServiceRequest(requestId);
+        return  serviceRequest;
+    }
+
+    public int getNumberOfServiceRequest(){
+        return requestList.getServiceRequestCount();
+    }
+
+    public List<ServiceRequest> getServiceRequestList() {
+        return requestList.getServiceRequestList();
+    }
+
+    public void assignTechnician(String name){
+        if (serviceRequest.getTechnician() == null) {
+            serviceRequest.setTechnician(new Technician(name));
+        } else throw new UnsupportedOperationException("Only one technician is allowed for one service request");
+    }
+}
---
+++ src/main/java/service/domain/ServiceRequestList.java
@@ -0,0 +1,35 @@
+package java.service.domain;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ServiceRequestList implements IServiceRequestList {
+    private List<ServiceRequest> serviceRequests;
+    @Override
+    public void addServiceRequest(Client client, String date) {
+        serviceRequests.add(new ServiceRequest(client, date));
+    }
+
+    /**
+     * Find the first matched  of {@link ServiceRequest}
+     * @param requestId Unique id of {@link ServiceRequest}
+     * @return {@link ServiceRequest}
+     */
+    @Override
+    public ServiceRequest searchServiceRequest(String requestId) {
+        return serviceRequests
+                .stream()
+                .filter(serviceRequest -> serviceRequest.getId().equals(requestId))
+                .collect(Collectors.toList()).get(0);
+    }
+
+    @Override
+    public int getServiceRequestCount() {
+        return serviceRequests.size();
+    }
+
+    @Override
+    public List<ServiceRequest> getServiceRequestList() {
+        return serviceRequests;
+    }
+}
